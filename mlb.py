47971130de27adfaf13acb8e181537092643c4ff
JoshNas <36458322+JoshNas@users.noreply.github.com>
2019-08-04 13:59:50 -0400

    NFL

--- mlb.py
+++ mlb.py
@@ -4,33 +4,8 @@ import datetime as dt
 import get_moneylines as gm
 
 
-def get_moneylines():
-    """Scrape MLB moneylines from SBR"""
-    url = 'https://classic.sportsbookreview.com/betting-odds/money-line/'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    game_days = soup.find_all('div', attrs={'class': "eventGroup class-mlb-baseball show-rotation"})
-
-    money_lines = []
-
-    for game_day in game_days:
-        game_date = get_game_date(game_day)
-        if game_date:
-            games = game_day.find_all('div', attrs={'class': 'event-holder holder-scheduled'})
-
-            for game in games:
-                teams = game.find_all('a')
-                away_team = teams[1].get_text()[:3].strip()
-                home_team = teams[2].get_text()[:3].strip()
-                time = game.find('div', attrs={'class': 'el-div eventLine-time'}) \
-                    .find('div', attrs={'class': "eventLine-book-value"}).get_text()
-
-                lines = gm.moneyline(game)
-                game_info = f'{away_team} vs {home_team} {game_date} {time}'
-                line = {'game': game_info, 'away_odds': lines[0], 'home_odds': lines[1]}
-                money_lines.append(line)
-    for l in money_lines:
-        print(l)
+rl = 'https://classic.sportsbookreview.com/betting-odds/mlb-baseball/pointspread/'
+tots = 'https://classic.sportsbookreview.com/betting-odds/mlb-baseball/totals/'
 
 
 def get_game_date(game):
@@ -48,4 +23,63 @@ def get_game_date(game):
 
     return game_date
 
-get_moneylines()
+
+def get_games(url):
+    """Scrape MLB moneylines from SBR"""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    game_days = soup.find_all('div', attrs={'class': "eventGroup class-mlb-baseball show-rotation"})
+    return game_days
+
+
+def get_moneylines():
+    """Scrape MLB moneylines from SBR"""
+    url = 'https://classic.sportsbookreview.com/betting-odds/money-line/'
+    game_days = get_games(url)
+
+    money_lines = []
+
+    for game_day in game_days:
+        game_date = get_game_date(game_day)
+        if game_date:
+            games = game_day.find_all('div', attrs={'class': 'event-holder holder-scheduled'})
+
+            for game in games:
+                teams = get_game_info(game)
+                game_info = f'{teams} {game_date}'
+                lines = gm.moneyline(game)
+                line = {'game': game_info, 'away_odds': lines[0], 'home_odds': lines[1]}
+                money_lines.append(line)
+
+    return money_lines
+
+
+def get_game_info(game):
+    """Get teams and time"""
+    teams = game.find_all('a')
+    away_team = teams[1].get_text()[:3].strip()
+    home_team = teams[2].get_text()[:3].strip()
+    time = game.find('div', attrs={'class': 'el-div eventLine-time'}) \
+        .find('div', attrs={'class': "eventLine-book-value"}).get_text()
+    return f'{away_team} vs {home_team} {time}'
+
+
+def get_runlines():
+    url = 'https://classic.sportsbookreview.com/betting-odds/money-line/'
+    game_days = get_games(url)
+
+    run_lines = []
+
+    for game_day in game_days:
+        game_date = get_game_date(game_day)
+        if game_date:
+            games = game_day.find_all('div', attrs={'class': 'event-holder holder-scheduled'})
+
+            for game in games:
+                teams = get_game_info(game)
+                game_info = f'{teams} {game_date}'
+
+
+mls = get_moneylines()
+for ml in mls:
+    print(ml)
---
+++ nfl.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import get_moneylines as gm
+
+
+def nfl_money_lines():
+    money_lines = []
+
+    url = 'https://classic.sportsbookreview.com/betting-odds/nfl-football/money-line/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    games = soup.find_all('div', attrs={'class': 'event-holder holder-scheduled'})
+
+    for game in games:
+        teams = game.find_all('a')
+        try:
+            away_team = teams[1].get_text()
+            home_team = teams[2].get_text()
+        except IndexError:
+            # Happens when change dates. ex: gap between Sunday and Monday games
+            break
+        lines = gm.moneyline(game)
+        game_info = f'{away_team} vs {home_team}'
+        line = {'game': game_info, 'away_odds': lines[0], 'home_odds': lines[1]}
+        money_lines.append(line)
+
+    return money_lines
